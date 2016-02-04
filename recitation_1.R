b4e820b9b4d7470f2622b73b152cf52eb9842c90
kmunger <km2713@nyu.edu>
2016-02-04 15:24:43 -0500

    updated Recitation 1

--- recitation_1.R
+++ recitation_1.R
@@ -4,50 +4,87 @@
 ## Avaliable on his website: www.kenbenoit.net
 
 
-
-
-
-
-
-
-
-###Make sure you have devtools installed
-install.packages("devtools")
-library(devtools)
 ## be sure to install the latest version from GitHub, using dev branch:
-devtools::install_github("quanteda", username="kbenoit", dependencies=FALSE, ref="dev")
-install.packages("quanteda")
+devtools::install_github("quanteda", username="kbenoit", dependencies=TRUE, ref="dev")
 ## and quantedaData
 devtools::install_github("quantedaData", username="kbenoit")
 
 library(quanteda)
-
-## stemming examples
+library(quantedaData)
+##start with a short document
 sampletxt <- "The police with their policing strategy instituted a policy of general 
 iterations at the Data Science Institute."
-wordstem(tokenize(sampletxt, simplify=TRUE))
 
-# stopwords examples
-library(quantedaData)
-data(exampleString)
-topfeatures(dfm(exampleString, verbose=FALSE))
-topfeatures(dfm(exampleString, ignoredFeatures=stopwords("english"), verbose=FALSE))
-topfeatures(dfm(exampleString, ignoredFeatures=stopwords("english"), stem=TRUE, verbose=FALSE))
+#Let's tokenize
+tokens<-tokenize(sampletxt)
+?tokenize
+
+tokens<-tokenize(sampletxt, removePunct=TRUE)
 
 
+## stemming examples
+
+stems<-wordstem(tokens)
+?wordstem
+
+# vector representations
+
+data("SOTUCorpus")
+
+speeches<-data.frame(SOTUCorpus$documents)
+
+last_speech_text<-speeches$texts[230]
+
+obama_dfm<-dfm(last_speech_text)
+?dfm
+
+##stopwords example
+
+stopwords("english")
+
+obama_dfm<-dfm(last_speech_text, ignoredFeatures = stopwords("english"))
+
+
+###full matrix of all speeches
+
+full_dfm<-dfm(speeches$texts, ignoredFeatures = stopwords("english"))
+
+topfeatures(full_dfm)
+
+
+##weighting--tfidf
+
+
+weighted<-tfidf(full_dfm)
+
+
+topfeatures(weighted)
+
+##Think about what this means!
+
+
+
+normalized<-tfidf(full_dfm, normalize=TRUE)
+
+topfeatures(normalized)
 
 ## constructing bigrams
-collocations(sampletxt, size = 2 )
-collocations(sampletxt, size = 3 )
+collocations(last_speech_text)
 
-## detect collocations in UK party manifestos speeches
-library(quantedaData)
-data(ukManifestos)
-colloc <- collocations(subset(ukManifestos, Year==2001))
-stopwords <- sort(stopwords("english"))
+
+?collocations
+
+collocations(last_speech_text, size=3)
+
+
+
+## detect collocations in overall
+
+colloc <- collocations(last_speech_text)
+
 
 ## remove any collocations containing a word in the stoplist
-stopwordindex <- which(colloc$word1 %in% stopwords | colloc$word2 %in% stopwords)
-colloc[stopwordindex, ]   # collocations containing stopwords
-colloc[-stopwordindex,]  # collocations not containing stopwords
+isStopwordList <- lapply(colloc$word1, `%in%`, stopwords("english"))
+stopwordindex <- which(colloc$word1 %in% stopwords("english")| colloc$word2 %in% stopwords("english"))
+colloc[-stopwordindex]  # collocations not containing stopwords
 
